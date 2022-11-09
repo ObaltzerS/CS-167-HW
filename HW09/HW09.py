ee4e31830a258cbd629716e3cc9922cd888a18e4
Oliver baltzer <114311552+ObaltzerS@users.noreply.github.com>
2022-11-09 13:35:43 -0800

    Add files via upload

--- HW09/HW09.py
+++ HW09/HW09.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov  6 14:27:24 2022
-
-@author: baltzero
-
-paxton luther
+Computer Science 167 
+Homework #9
+November 8, 2022
+Authors: Oliver Baltzer, Maxwell Montgomery
 """
 import textlib
 import urllib.request as web
@@ -37,7 +36,8 @@ def webWordCount(url):
         An integer corrosponding to the number of words counted 
     """
     urltxt = web.urlopen(url)
-    return textlib.wordCount(urltxt.read())
+    txt = urltxt.read()
+    return textlib.wordCount(str(txt))
     
 def string2Digit(digitString):
     """
@@ -63,9 +63,10 @@ def isHTML(url):
         
     """
     urltxt = web.urlopen(url)
-    urltxt = urltxt.read()
+    txt = urltxt.read()
+    txt = str(txt)
     htmlDec = "doctype html"
-    return (htmlDec in textlib.normalize(urltxt)) 
+    return (htmlDec in textlib.normalize(txt)) 
     
 def username(first, last):
     """
@@ -96,7 +97,7 @@ def replace(text, target, replacement):
     start = 0
     for i in range(len(text)):
         if text[i:i + len(target)] == target:
-            newText += text[start:i] + str(replacement)
+            newText = newText + text[start:i] + str(replacement)
             start = i + len(replacement)
     return newText
 
@@ -112,7 +113,7 @@ def longestRun(text):
     """
     prevChar = ""
     run = 1
-    longRun = 0
+    longRun = 1
     for char in text:
         if char == prevChar:
             run += 1 
@@ -165,39 +166,76 @@ def uniqueCharacters(text):
 
 
 def test_reverse():
-    # test cases 
+    assert reverse('download') == 'daolnwod'
+    assert reverse("two words") == "sdrow owt"
+    assert reverse("was it a rat I saw") == "was I tar a ti saw"
+    assert reverse("1234") == "4321"
+    assert reverse("P") == "P"
     return "reverse() passes all tests"
     
 def test_webWordCount():
     
+    assert webWordCount("https://www.gutenberg.org/cache/epub/2701/pg2701-images.html") >= 209117
+    assert webWordCount("https://www.gutenberg.org/cache/epub/996/pg996-images.html") >= 345390
+    assert webWordCount("https://www.gutenberg.org/cache/epub/69315/pg69315-images.html") == 77691
+    assert webWordCount("https://www.gutenberg.org/cache/epub/69310/pg69310-images.html") == 58492
+    assert webWordCount("https://www.gutenberg.org/cache/epub/69308/pg69308-images.html") == 13388
     return "webWordCount() passes all tests"
     
 def test_string2Digit():
-    
+    assert string2Digit("5") == 5
+    assert string2Digit("9") == 9
+    assert string2Digit("0") == 0
+    assert string2Digit("1") == 1
+    assert string2Digit("6") == 6
     return "string2Digit() passes all tests"
 
 def test_isHTML():
-    
+    assert isHTML("https://www.gutenberg.org/cache/epub/2701/pg2701-images.html") == True
+    assert isHTML("https://www.gutenberg.org/cache/epub/996/pg996-images.html") == True
+    assert isHTML("https://www.gutenberg.org/cache/epub/69308/pg69308-images.html") == True
+    assert isHTML("https://www.gutenberg.org/cache/epub/69310/pg69310-images.html") == True
+    assert isHTML("https://www.gutenberg.org/cache/epub/69315/pg69315-images.html") == True
     return "isHTML() passes all tests"
     
 def test_username():
-    
+    assert username('martin', 'freeman') == 'freeman_m'
+    assert username("Morgan", "Freeman") == "Freeman_M"
+    assert username("Oliver", "Baltzer") == "Baltzer_O"
+    assert username("Maxwell", "Montgomery") == "Montgomery_M"
+    assert username("user", "name44") == "name44_u"
     return "username() passes all tests"
     
 def test_replace():
-    
+    assert replace('I am cool, very cool.', 'cool', 'very vain') == 'I am very vainy very vain'
+    assert replace("word", "word", "cool") == "cool"
+    assert replace("replace this word", "word", "replaced") == "replace this replaced"
+    assert replace("I am a cowboy", "cowboy", "wizard") == "I am a wizard"
+    assert replace("abcdefg", "c", "f") == "abfdefg" 
     return "replace() passes all tests"
     
 def test_longestRun():
-    
+    assert longestRun("aabbbbcccd") == 4
+    assert longestRun("a") == 1
+    assert longestRun("woords wiiith reeeepeated chhhhharacters") == 5
+    assert longestRun("1234") == 1
+    assert longestRun("words with no repeats") == 1
     return "longestRun() passes all tests"
     
 def test_difference():
-    
+    assert difference('bob', 'bob') == -1
+    assert difference('bob', 'sallyann') == 0
+    assert difference("auto", "automobile") == 4
+    assert difference("automobile", "auto") == 4
+    assert difference("1234", "1235") == 3
     return "difference() passes all tests"
     
 def test_uniqueCharacters():
-    
+    assert uniqueCharacters('aaeeioou') == 2
+    assert uniqueCharacters('bobby') == 2
+    assert uniqueCharacters('aabbcc') == 0
+    assert uniqueCharacters("words with unique characters") == 4
+    assert uniqueCharacters("abcde") == 5
     return "uniqueCharacters() passes all tests "
 
 def test():
@@ -206,7 +244,7 @@ def test():
     print(test_string2Digit())
     print(test_isHTML())
     print(test_username())
-    print(test_replace())
+    print(test_replace()) # replace function is faulty, wasnt able to come up with a solution
     print(test_longestRun())
     print(test_difference())
     print(test_uniqueCharacters())
@@ -214,16 +252,3 @@ def test():
 
 if __name__ == "__main__":
     test()
-    
-    
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
